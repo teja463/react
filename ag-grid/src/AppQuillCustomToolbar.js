2d03777a6c4757232954a41c263796b4a123f3dd
Brahma Teja Ponnuru <teja463@gmail.com>
2023-04-28 17:17:26 +0530

    moved editors to new components

---
+++ ag-grid/src/AppQuillCustomToolbar.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Editor from "./EditroWithCustomToolbar";
+
+export default function AppQuill() {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const [list, setList] = useState([{ id: generateString() }]);
+
+  function generateString(length = 5) {
+    let result = "_";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+  }
+
+  function addEditor() {
+    const ne = { id: generateString() };
+    setList([...list, ne]);
+  }
+
+  function deleteEditor(id){
+    console.log('deleting', id)
+    setList(list.filter(l => l.id !== id))
+  }
+
+  return (
+    <div className="App">
+      <button onClick={addEditor}>Add</button>
+      {list.map((m) => (
+        <div key={m.id}>
+          <Editor eid={m.id} deleteEditor={deleteEditor}/>
+        </div>
+      ))}
+    </div>
+  );
+}
--- ag-grid/src/index.js
+++ ag-grid/src/index.js
@@ -3,7 +3,9 @@ import { render } from "react-dom";
 import "./App.css";
 // import App from './App';
 // import App from './AppOld';
-import App from "./AppQuillInbuiltModules";
+// import App from "./AppQuillInbuiltModules";
+import App from "./AppQuillInbuiltModulesUndo";
+// import App from "./AppQuillCustomToolbar";
 // import App2 from "./AppQuill2";
 
 render(
